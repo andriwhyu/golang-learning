d8c8672ae0cb10bef82268b4cb64bb40a57a7012
andriruslam <andri.wahyu@accelbyte.net>
2024-01-15 21:01:13 +0700

    feat: array exercise set array and elipsis

---
+++ array/exercises/elipsis-04/README.md
@@ -0,0 +1,8 @@
+## EXERCISE: Refactor to Ellipsis
+1. Use the get-set-array-02 exercise
+2. Refactor the length of the array literals to ellipsis
+
+This means: Use the ellipsis instead of defining the array's length manually.
+
+## EXPECTED OUTPUT
+The output should be the same as the get-set-array-02 exercise.
---
+++ array/exercises/get-set-array-02/main.go
@@ -0,0 +1,102 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func main() {
+	var (
+		names     = [3]string{"Einstein", "Tesla", "Shepard"}
+		distances = [5]int{50, 40, 75, 30, 125}
+		data      = [5]uint{72, 69, 76, 76, 79}
+		ratio     = [1]float64{3.14}
+		alives    = [4]bool{false, true, true, false}
+		zero      = [0]uint8{}
+	)
+
+	separator := "\n" + strings.Repeat("=", 20) + "\n"
+
+	// Without loop range
+	fmt.Print("\nnames", separator)
+	for i := 0; i < len(names); i++ {
+		fmt.Printf("names[%d]: %s\n", i, names[i])
+	}
+	fmt.Println()
+
+	fmt.Println("distances\n====================")
+	for i := 0; i < len(distances); i++ {
+		fmt.Printf("distances[%d]: %d\n", i, distances[i])
+	}
+	fmt.Println()
+
+	fmt.Println("data\n====================")
+	for i := 0; i < len(data); i++ {
+		fmt.Printf("data[%d]: %d\n", i, data[i])
+	}
+	fmt.Println()
+
+	fmt.Println("ratio\n====================")
+	for i := 0; i < len(ratio); i++ {
+		fmt.Printf("ratio[%d]: %.2f\n", i, ratio[i])
+	}
+	fmt.Println()
+
+	fmt.Println("alives\n====================")
+	for i := 0; i < len(alives); i++ {
+		fmt.Printf("alives[%d]: %t\n", i, alives[i])
+	}
+	fmt.Println()
+
+	fmt.Println("zero\n====================")
+	for i := 0; i < len(zero); i++ {
+		fmt.Printf("zero[%d]: %d\n", i, zero[i])
+	}
+	fmt.Println()
+
+	//fmt.Println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\nFOR RANGES\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n")
+	fmt.Printf(`
+%s
+FOR RANGES
+%[1]s
+
+`, strings.Repeat("~", 30))
+
+	// Range
+	fmt.Println("names\n====================")
+	for i, val := range names {
+		fmt.Printf("names[%d]: %s\n", i, val)
+	}
+	fmt.Println()
+
+	fmt.Println("distances\n====================")
+	for i, val := range distances {
+		fmt.Printf("distances[%d]: %d\n", i, val)
+	}
+	fmt.Println()
+
+	fmt.Println("data\n====================")
+	for i, val := range data {
+		fmt.Printf("data[%d]: %d\n", i, val)
+	}
+	fmt.Println()
+
+	fmt.Println("ratio\n====================")
+	for i, val := range ratio {
+		fmt.Printf("ratio[%d]: %.2f\n", i, val)
+	}
+	fmt.Println()
+
+	fmt.Println("alives\n====================")
+	for i, val := range alives {
+		fmt.Printf("alives[%d]: %t\n", i, val)
+	}
+	fmt.Println()
+
+	fmt.Println("zero\n====================")
+	for i, val := range zero {
+		fmt.Printf("zero[%d]: %d\n", i, val)
+	}
+	fmt.Println()
+
+}
