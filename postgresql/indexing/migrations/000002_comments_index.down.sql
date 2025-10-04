8869af3627516728fa31d0bc156a7cbd9a282315
andriruslam <andri.wahyu@accelbyte.net>
2025-10-04 18:08:16 +0700

    feat: add gin indexing testing

---
+++ postgresql/indexing/migrations/000002_comments_index.down.sql
@@ -0,0 +1,1 @@
+DROP INDEX IF EXISTS comments_username_idx;
---
+++ postgresql/indexing/migrations/000003_comments_content_index.up.sql
@@ -0,0 +1,3 @@
+CREATE EXTENSION IF NOT EXISTS pg_trgm;
+
+CREATE INDEX IF NOT EXISTS comments_content_trgm_idx ON comments USING GIN (content gin_trgm_ops);
